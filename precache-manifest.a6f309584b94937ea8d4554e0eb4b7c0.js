5f82356399a31218e08bb5531fd9002a7dc095c9
rd-goncalves <raphaelnoda@hotmail.com>
2021-05-05 22:22:14 -0300

    Updates

---
+++ precache-manifest.a6f309584b94937ea8d4554e0eb4b7c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6054d8d0faabeaf664b9",
+    "url": "/LightsOut/static/css/main.81a5b700.chunk.css"
+  },
+  {
+    "revision": "6054d8d0faabeaf664b9",
+    "url": "/LightsOut/static/js/main.6054d8d0.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/LightsOut/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "135e11432d80503c488c",
+    "url": "/LightsOut/static/js/runtime~main.135e1143.js"
+  },
+  {
+    "revision": "f8d36fa73d82e50dd3066b61c4aa95bb",
+    "url": "/LightsOut/index.html"
+  }
+];
